9df29fbb67e6a3f45fa6dd2835fce36186615552
MichJRC <michael.assouline@ec.europa.eu>
2025-10-07 16:54:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,9 +27,13 @@ wget -q --show-progress -O downloaded_data/parcels_with_HRL_codes.gpkg \
   https://github.com/MichJRC/gsa-parcels-viewer/releases/download/v1.0.0/parcels_with_HRL_codes.gpkg
 
 # Download HRL tiles
-#echo "Downloading HRL-tiles"
-#wget -q --show-progress -O downloaded_data/parcels_with_HRL_codes.gpkg \
-#  https://github.com/MichJRC/gsa-parcels-viewer/releases/download/v1.0.0/parcels_with_HRL_codes.gpkg
+echo "Downloading HRL-tiles"
+wget -q --show-progress -O downloaded_data/hrl_tiles_mosaic.tar.gz \
+  https://github.com/MichJRC/gsa-parcels-viewer/releases/download/v1.0.0/hrl_tiles_mosaic.tar.gz
+
+# Unzip the tiles
+echo "Extracting the tiles..."
+cd downloaded_data && unzip -q -o hrl_tiles_mosaic.tar.gz && cd ..
 
 # Verify all files were downloaded
 echo ""
@@ -38,6 +42,7 @@ FILES=(
     "downloaded_data/Appe_Azi_PCG_2021_FE.zip"
     "downloaded_data/parcels_cleaned.gpkg"
     "downloaded_data/parcels_with_HRL_codes.gpkg"
+    "downloaded_data/hrl_tiles_mosaic.tar.gz"
 )
 
 ALL_PRESENT=true
@@ -59,6 +64,7 @@ if [ "$ALL_PRESENT" = true ]; then
     echo "  1. Appe_Azi_PCG_2021_FE/ - Original shapefile"
     echo "  2. parcels_cleaned.gpkg - Cleaned parcels"
     echo "  3. parcels_with_HRL_codes.gpkg - HRL-classified parcels (ready to use)"
+    echo "  4. hrl_tiles_mosaic.tar.gz - hrl_tiles_mosaic (ready to use)"
 else
     echo "❌ Setup incomplete - some files are missing"
     exit 1
