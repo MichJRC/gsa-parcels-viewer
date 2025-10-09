2f6b09a45aa7749263095d1ec544316d34bbce7c
MichJRC <michael.assouline@ec.europa.eu>
2025-10-09 12:32:07 +0000

    python 3.13

--- pre_process_scripts/extracting_info_gpgk.py
+++ pre_process_scripts/extracting_info_gpgk.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
-
+import matplotlib.pyplot as plt
 import pandas
 
 gdf = gpd.read_file("downloaded_data/parcels_with_HRL_codes.gpkg")
@@ -10,3 +10,9 @@ print(col)
 # get the unique classes of hrl
 unique_combinations = gdf[['hrl_code', 'hrl_name']].drop_duplicates()
 print(f"Unique combinations:\n{unique_combinations}")
+
+
+gdf.plot(column="hrl_name", cmap='tab20')
+plt.title()
+plt.show()
+
