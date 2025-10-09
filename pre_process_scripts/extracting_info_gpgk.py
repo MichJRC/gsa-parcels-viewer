96b229237357706490b37e7b5b63eb0514a3d7eb
MichJRC <michael.assouline@ec.europa.eu>
2025-10-09 15:48:56 +0000

    having jupyter on python interactive to display maps

--- pre_process_scripts/extracting_info_gpgk.py
+++ pre_process_scripts/extracting_info_gpgk.py
@@ -16,41 +16,4 @@ print(col)
 unique_combinations = gdf[['hrl_code', 'hrl_name']].drop_duplicates()
 print(f"Unique combinations:\n{unique_combinations}")
 
-
 # %%
-import geopandas as gpd
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-print("Libraries loaded!")
-
-# %%
-# Matplotlib example
-data = pd.DataFrame({
-    'x': [1, 2, 3, 4, 5],
-    'y': [2, 4, 6, 8, 10]
-})
-
-plt.figure(figsize=(10, 6))
-plt.plot(data['x'], data['y'], marker='o')
-plt.title('My Plot')
-plt.xlabel('X axis')
-plt.ylabel('Y axis')
-plt.grid(True)
-plt.show()
-
-# %%
-# Plotly interactive plot
-fig = px.scatter(data, x='x', y='y', title='Interactive Plot')
-fig.show()
-
-# %%
-# GeoPandas map example (when you have geodata)
-# world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-# world.plot(figsize=(15, 10))
-# plt.title('World Map')
-# plt.show()
-
-
-
