641928dc54a42f8debdc65eced393b0722b5ca9a
MichJRC <michael.assouline@ec.europa.eu>
2025-10-20 09:52:14 +0000

    back to port 5000

--- flask_tile_viewer/app.py
+++ flask_tile_viewer/app.py
@@ -435,4 +435,4 @@ if __name__ == '__main__':
     
     # Disable reloader to avoid issues
     # You'll need to manually restart after code changes
-    app.run(debug=True, host='0.0.0.0', port=8000, threaded=True, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
